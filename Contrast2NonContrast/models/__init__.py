917f086a748a13c93cb38ead9e7471fa5694b3a7
Qiyu Zhang <148504334+Qiyu-Zh@users.noreply.github.com>
2024-10-09 12:09:01 -0700

    Update __init__.py

--- Contrast2NonContrast/models/__init__.py
+++ Contrast2NonContrast/models/__init__.py
@@ -29,7 +29,7 @@ def find_model_using_name(model_name):
     be instantiated. It has to be a subclass of BaseModel,
     and it is case-insensitive.
     """
-    model_filename = "models." + model_name + "_model"
+    model_filename = "Contrast2NonContrast.models." + model_name + "_model"
     # model_filename = 'models.ivit_gan_model'
     # model_filename = 'models.cytran_model'
 
