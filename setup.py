d5b1e741cd04cc57ad44c0ea31cf3464f5b40e79
Qiyu Zhang <148504334+Qiyu-Zh@users.noreply.github.com>
2024-10-10 13:37:29 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,25 @@
 import setuptools
-
-if __name__ == "__main__":
-    setuptools.setup()
+setup(
+    name='Contrast2NonContrast',
+    version="0.1.0",
+    description="A short description of your package",
+    packages=find_packages(),  # Automatically find and include your package
+    install_requires=[         # List of dependencies
+      "torch>=2.3.0",
+      "matplotlib",
+      "numpy",
+      "SimpleITK"
+    ],
+    package_data={
+        '': ['checkpoints/da-56/*', 'models/*', 'options/*', 'util/*']
+    },
+    dependency_links=[
+      "git+https://github.com/Qiyu-Zh/TotalSegmentator_Crop.git",
+      "git+https://github.com/Qiyu-Zh/Image-Toolbox.git"
+    ],
+    classifiers=[              # Metadata
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.9',
+)
 
